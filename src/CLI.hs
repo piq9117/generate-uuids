7c79e504e49a484195971887385a1e68b44a32ab
piq9117 <piq9117@gmail.com>
2024-05-14 20:34:36 -0400

    cli commands/parser
    start

---
+++ src/CLI.hs
@@ -0,0 +1,11 @@
+{-# language GADTs #-}
+
+module CLI where
+
+data Command a where
+  ParseCommand :: Text -> Command CliInput
+
+data CliInput = CliInput 
+  { filePath :: FilePath,
+    count :: Int
+  }
