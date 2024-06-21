18a2a3ae0aabb5bc98fc6df91d70d020d0ddecfc
piq9117 <piq9117@gmail.com>
2024-06-21 00:22:23 -0400

    use nix developPackage

--- flake.nix
+++ flake.nix
@@ -23,9 +23,23 @@
         '';
       };
 
+      packages = forAllSystems(system: 
+        let 
+          pkgs = nixpkgsFor.${system};
+          developPackage = pkgs.hsPkgs.developPackage {
+            root = ./.;
+          };
+        
+        in {
+          default = developPackage;
+        });
+
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
+          developPackage = pkgs.hsPkgs.developPackage {
+            root = ./.;
+          };
           libs = with pkgs; [
             zlib
           ];
@@ -42,6 +56,7 @@
               nixpkgs-fmt
               hsPkgs.cabal-fmt
               init-project
+              developPackage
             ] ++ libs;
             shellHook = "export PS1='[$PWD]\n❄ '";
             LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
